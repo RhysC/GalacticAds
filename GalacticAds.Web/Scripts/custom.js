fa332ecfa00e1ce43ea97d7c40f6fd7bc676ac84
Rhys Campbell <rhysevancampbell@gmail.com>
2010-07-29 22:32:19 +0800

    not sure why the files wernt added on last commit

---
+++ GalacticAds.Web/Scripts/custom.js
@@ -0,0 +1,7 @@
+﻿function globalAjaxCursorChange() {
+    $("html").bind("ajaxStart", function () {
+        $(this).addClass('busy');
+    }).bind("ajaxStop", function () {
+        $(this).removeClass('busy');
+    });
+}
---
+++ GalacticAds.Web/Views/Store/Summary.ascx
@@ -0,0 +1,5 @@
+﻿<%@ Control Language="C#" Inherits="System.Web.Mvc.ViewUserControl<GalacticAds.Web.Models.Store>" %>
+<div>
+    <h2><%: Model.Name %></h2>
+    <%:Model.GeographicalLocation.ToString() %>
+</div>
